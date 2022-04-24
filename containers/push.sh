4c5c6544b33f651ed37fd3ebe6d80e737b54d34b
Geoffrey Challen <challen@illinois.edu>
2022-04-24 13:15:52 -0500

    New build files.

--- containers/push.sh
+++ containers/push.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-for d in ./*/ ; do (cd "$d" && docker push cs124/playground-runner-${PWD##*/}); done
+for d in ./*/ ; do (cd "$d" && docker buildx build --platform=linux/amd64,linux/arm64 . --push -t cs124/playground-runner-${PWD##*/}:latest); done
